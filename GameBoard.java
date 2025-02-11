d8aa768d7d31708e1abfc7ad06932fe486751350
Steveesunhy0923 <stevesunhy@gmail.com>
2025-02-11 10:56:26 -0800

    final update

--- GameBoard.java
+++ GameBoard.java
@@ -8,7 +8,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class GameBoard extends World
 {
-    private final int NUM_CARDS_IN_DECK = 81;
+    private final int NUM_CARDS_IN_DECK = 27;
     
     public GameBoard()
     {   
